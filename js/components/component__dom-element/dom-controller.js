8c533cbc78810baa152cda304d21447940af23b5
mnaatjes <michael.naatjes87@gmail.com>
2024-12-05 11:54:05 -0500

    created dom controller branch

---
+++ js/components/component__dom-element/dom-controller.js
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------*/
+/**
+ * @file src/js/components/dom-controller.js
+ * @author mnaatjes
+ * @version 1.0.0
+ * @date 11-07-2024
+ * 
+ * @name DOMController
+ * @type {Class}
+ * @memberof Src.Components
+ * @namespace DOMController
+ * 
+ * @property {HTMLElement} node
+ * @property {Object} state
+ */
+/*----------------------------------------------------------*/
+/*----------------------------------------------------------*/
+/**
+ * @name ElementState
+ * @type {Class}
+ * @memberof Src.Components
+ * @property {}
+ * @description
+ */
+/*----------------------------------------------------------*/
+class ElementState {
+    constructor(){
+    }
+    /*----------------------------------------------------------*/
+    /**
+     * @name current
+     * @type {}
+     * @memberof ElementState
+     * @desciption
+     */
+    /*----------------------------------------------------------*/
+    get current(){}
+    set current(value){}
+    /*----------------------------------------------------------*/
+    /**
+     * @name 
+     * @type {}
+     * @memberof ElementState
+     * @desciption
+     */
+    /*----------------------------------------------------------*/
+}
