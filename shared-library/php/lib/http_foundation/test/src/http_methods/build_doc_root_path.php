f2976584bf73fd2e3d20b458597d5a1dbb1ea171
mnaatjes <michael.naatjes87@gmail.com>
2025-04-06 12:58:04 -0400

    update

--- shared-library/php/lib/http_foundation/test/src/http_methods/build_doc_root_path.php
+++ shared-library/php/lib/http_foundation/test/src/http_methods/build_doc_root_path.php
@@ -3,6 +3,12 @@
      * Builds a filepath relative to the $_SERVER['DOCUMENT_ROOT'].
      * 
      * @param string $relative_path - The filepath
-     * @return string|null - 
+     * @return string|null - Built relative path | Null if failed
      */
+    function build_doc_root_path(string $relative_path) {
+        /**
+         * Validate that $_SERVER['DOCUMENT_ROOT'] is set
+         */
+        $doc_root = get_document_root();
+    }
 ?>
